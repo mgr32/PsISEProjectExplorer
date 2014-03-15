a22fa515320fcadd6a6fa15cec70a387b4694eba
mgr32 <marcin.grzywa@gmail.com>
2014-03-15 23:27:54 +0100

    Added icons, removed Lucene contrib.

--- ProjectExplorer/Properties/Resources.Designer.cs
+++ ProjectExplorer/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace ProjectExplorer.Properties {
+    using System;
     
     
     /// <summary>
@@ -37,7 +38,7 @@ namespace ProjectExplorer.Properties {
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProjectExplorer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
