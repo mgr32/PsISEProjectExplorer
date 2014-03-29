731bf0b73d4b3e3d6e7d0ea88cbd5dd5a27a5829
mgrzywa <marcin.grzywa@gmail.com>
2014-03-29 08:03:56 +0100

    Bugfixes.

---
+++ PsISEProjectExplorer/Model/FileSystemChangedInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsISEProjectExplorer.Model
+{
+    public class FileSystemChangedInfo
+    {
+        public IEnumerable<string> PathsChanged { get; private set; }
+
+        public FileSystemChangedInfo(IEnumerable<string> pathsChanged)
+        {
+            this.PathsChanged = pathsChanged;
+        }
+    }
+}
--- PsISEProjectExplorer/Services/FileSystemChangeNotifier.cs
+++ PsISEProjectExplorer/Services/FileSystemChangeNotifier.cs
@@ -1,4 +1,6 @@
-﻿using PsISEProjectExplorer.Model.DocHierarchy;
+﻿using NLog;
+using PsISEProjectExplorer.Model;
+using PsISEProjectExplorer.Model.DocHierarchy;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,9 +12,9 @@ namespace PsISEProjectExplorer.Services
 {
     public static class FileSystemChangeNotifier
     {
-        public static event FileSystemEventHandler FileSystemChanged;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
 
-        public static event RenamedEventHandler FileSystemRenamed;
+        public static event EventHandler<FileSystemChangedInfo> FileSystemChanged;
 
         private static FileSystemWatcher watcher = new FileSystemWatcher();
 
@@ -24,7 +26,7 @@ namespace PsISEProjectExplorer.Services
                 return;
             }
             watcher.Path = path;
-            watcher.NotifyFilter = NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.Size;
+            watcher.NotifyFilter = NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.CreationTime | NotifyFilters.Security;
             watcher.IncludeSubdirectories = true;
             watcher.Changed += new FileSystemEventHandler(OnFileChanged);
             watcher.Created += new FileSystemEventHandler(OnFileChanged);
@@ -35,12 +37,34 @@ namespace PsISEProjectExplorer.Services
 
         private static void OnFileChanged(object source, FileSystemEventArgs e)
         {
-            FileSystemChanged(source, e);
+            bool isDir = Directory.Exists(e.FullPath);
+            if (isDir && e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            if (!isDir && !FilesPatternProvider.POWERSHELL_FILES_REGEX.IsMatch(e.FullPath))
+            {
+                return;
+            }
+            FileSystemChangedInfo changedInfo = new FileSystemChangedInfo(new List<string>() { e.FullPath });
+            FileSystemChanged(source, changedInfo);
         }
 
         private static void OnFileRenamed(object source, RenamedEventArgs e)
         {
-            FileSystemRenamed(source, e);
+            IList<string> pathsChanged = new List<string>();
+            bool isDir = Directory.Exists(e.FullPath) || Directory.Exists(e.OldFullPath);
+            if (isDir || FilesPatternProvider.POWERSHELL_FILES_REGEX.IsMatch(e.OldFullPath))
+            {
+                pathsChanged.Add(e.OldFullPath);
+            }
+            if ((isDir || FilesPatternProvider.POWERSHELL_FILES_REGEX.IsMatch(e.FullPath)) &&
+                e.FullPath.ToLowerInvariant() != e.OldFullPath.ToLowerInvariant())
+            {
+                pathsChanged.Add(e.FullPath);
+            }
+            FileSystemChangedInfo changedInfo = new FileSystemChangedInfo(new List<string>() { e.FullPath });
+            FileSystemChanged(source, changedInfo);
         }
     }
 }
