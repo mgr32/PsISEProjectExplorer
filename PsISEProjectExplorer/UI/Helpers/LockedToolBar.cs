73b0cf544a04a95cbb2546f4ef80f1e3cfa46d2c
mgr <marcin.grzywa@gmail.com>
2015-10-11 17:31:14 +0200

    #27 - Togglebutton state hard to see. Wrapped toggle buttons in ToolBar.

---
+++ PsISEProjectExplorer/UI/Helpers/LockedToolBar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PsISEProjectExplorer.UI.Helpers
+{
+    public class LockedToolBar : ToolBar
+    {
+        public LockedToolBar()
+        {
+            Loaded += new RoutedEventHandler(LockedToolBar_Loaded);
+        }
+
+        private void LockedToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
