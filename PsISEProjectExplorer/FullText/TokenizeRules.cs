6037f0d921c534f56afdd01c66258ff813911d59
mgr <marcin.grzywa@gmail.com>
2015-10-12 17:57:34 +0200

    #57 - Search issue - terms with preceding bracket are not found. Reverted special characters treated as separators.

--- PsISEProjectExplorer/FullText/TokenizeRules.cs
+++ PsISEProjectExplorer/FullText/TokenizeRules.cs
@@ -4,9 +4,12 @@ namespace PsISEProjectExplorer.FullText
 {
     public class TokenizeRules
     {
+        private static readonly char[] TokenSeparators = { '.', ',', ':', '(', ')', '[', ']', '{', '}', '\\', '/', '`', '\'', '"' };
+
+        // note this is used both in CustomQueryParser (for parsing user input) and CustomTokenizer (for parsing document)
         public bool IsTokenChar(char c)
         {
-            return !System.Char.IsWhiteSpace(c) && !this.IsPhraseChar(c);
+            return !System.Char.IsWhiteSpace(c) && !TokenSeparators.Contains(c);
         }
 
         public bool IsPhraseChar(char c)
